240ede43ea7c5ff50b9df3f061350dbc162ef272
Shaon <shaon@Shaons-iMac.local>
2019-07-07 22:58:13 +0600

    Question with answer is ready

--- Quizzie/Model/QuestionBank.swift
+++ Quizzie/Model/QuestionBank.swift
@@ -11,10 +11,10 @@ struct QuestionBank {
     private var questions = [Question]()
     
     init() {
-        questions.append(Question(qs: "What’s the Target Platform of Kotlin?", answerSets: [""], correctAnsPosition: 2))
-        questions.append(Question(qs: "What’s init block in Kotlin?", answerSets: [""], correctAnsPosition: 2))
-        questions.append(Question(qs: "Is new a keyword in Kotlin??", answerSets: [""], correctAnsPosition: 2))
-        questions.append(Question(qs: "What’s a const?", answerSets: [""], correctAnsPosition: 2))
-        questions.append(Question(qs: "Kotlin is used for..", answerSets: [""], correctAnsPosition: 2))
+        questions.append(Question(qs: "What’s the Target Platform of Kotlin?", answerSets: ["JVM", "GraalVm", "CLR", "HotSpot"], correctAnsPosition: 1))
+        questions.append(Question(qs: "What’s init block in Kotlin?", answerSets: ["Finish something", "Initialiser block", "Exit from app", "Resume something"], correctAnsPosition: 2))
+        questions.append(Question(qs: "New keyword used in Kotlin?", answerSets: ["Initialize variable", "Open new page","No uses", "Used for initialize"], correctAnsPosition: 3))
+        questions.append(Question(qs: "What’s a const?", answerSets: ["A value cann't be changed later", "Value can be changed later", "Variable", "A Class"], correctAnsPosition: 1))
+        questions.append(Question(qs: "Kotlin is used for..", answerSets: ["Android", "Web", "IOS", "All of them"], correctAnsPosition: 4))
     }
 }
